135f702f50287cc0390187852ff0373fa9174603
ajith76 <ajithars20022gmail.com>
2023-09-13 10:24:15 +0530

    Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Task2Component } from './task2/task2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+  
+    Task2Component
   ],
   imports: [
     BrowserModule,
